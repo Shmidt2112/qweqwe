582ea441fbe704006dcc512b9fee0ffa8ad5d50b
Den Gorec <tpe3bbly1@gmail.com>
2015-05-28 06:17:43 +0000

    add function plus, minus

--- Ayamy App/scripts/app.js
+++ Ayamy App/scripts/app.js
@@ -1,9 +1,13 @@
 var app = (function () {
 
-    var mobileApp = {};
+    var mobileApp = {},
+        dataSource, price, total = 0,
+        count = 0,
+        countEl;
 
     var groupedData = [
         {
+            id: 0,
             name: "Sashimi Salad",
             description: "Зелень, украшеная свежими сашими, васаби, соевый винегрет.",
             url: "images/sashimi-salad.jpg",
@@ -11,6 +15,7 @@ var app = (function () {
             letter: "Роллы"
             },
         {
+            id: 1,
             name: "Seaweed Salad bla bla bla",
             description: "Салат из морепродуктов.",
             url: "images/seaweed-salad.jpg",
@@ -18,6 +23,7 @@ var app = (function () {
             letter: "Роллы"
             },
         {
+            id: 2,
             name: "Edamame",
             description: "Вареные соевые бобы с солью.",
             url: "images/edamame.jpg",
@@ -25,6 +31,7 @@ var app = (function () {
             letter: "Роллы"
             },
         {
+            id: 3,
             name: "Maguro",
             description: "Кусочки тунца.",
             url: "images/maguro.jpg",
@@ -32,6 +39,7 @@ var app = (function () {
             letter: "Роллы"
             },
         {
+            id: 4,
             name: "Tekka Maki",
             description: "Тунец-ролл с васаби.",
             url: "images/tekka-maki.jpg",
@@ -39,6 +47,7 @@ var app = (function () {
             letter: "Роллы"
             },
         {
+            id: 5,
             name: "California",
             description: "Крабовые палочки, авокадо и огурец.",
             url: "images/california-rolls.jpg",
@@ -48,10 +57,15 @@ var app = (function () {
     ];
 
     function initialize() {
+
         mobileApp = new kendo.mobile.Application(document.body, {
             skin: "flat"
         });
-    };
+
+        dataSource = new kendo.data.DataSource({
+            data: groupedData
+        });
+    }
 
     function initMain() {
         $("#main-list").kendoMobileListView({
@@ -64,13 +78,70 @@ var app = (function () {
         });
     }
 
+    function plus(e) {
+        //Получаем текущий элемент с кол-вом
+        countEl = $(e.target).prev();
+        //Получаем текущее кол-во
+        count = +$(e.target).prev().text();
+        price = +$(e.target).prev().data("price");
+        //Прибавляем 1
+        count++;
+        //Заносим в текущий элемент
+        $(e.target).prev().text(count);
+        //Вычитаем из общей стоимости текущий товар
+        total += price;
+        // //Заносим с текущим кол-вом
+        //total += price * count;
+        $("#basket").text(total);
+    }
+
+    function minus(e) {
+        countEl = $(e.target).next();
+        count = +$(e.target).next().text();
+        price = +$(e.target).next().data("price");
+        if (count > 1) {
+            count--;
+            $(e.target).next().text(count);
+            //Вычитаем из общей стоимости текущий товар
+            total -= price;
+            $("#basket").text(total);
+        }
+    }
+
+    var purchase = [];
+    var item = {};
+
+    function doPrice(e) {
+
+        //Текущая цена
+        price = $(e.target).data("price");
+        //Если цена выделена
+        if (!$(e.target).data("on")) {
+            //Общая стоимость
+            total += price;
+            $(e.target).data("on", true);
+        } else {
+            if (total - price * count > 0) {
+                total -= price * count;
+                $(countEl).text(1);
+            } else {
+                total = 0;
+            }
+            $(e.target).data("on", false);
+        }
+
+        $("#basket").text(total);
+        $(e.target).toggleClass("prices-clicked");
+        $("#" + $(e.target).data("id")).slideToggle("normal");
+    }
+
     document.addEventListener("deviceready", initialize);
 
     return {
         init: initMain,
-        doprice: function () {
-
-        }
+        doPrice: doPrice,
+        p: plus,
+        m: minus
     }
 
 }());
