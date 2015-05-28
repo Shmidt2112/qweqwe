ce8d09c4a777cecb73ac213ed6e7980531c89e42
Den Gorec <tpe3bbly1@gmail.com>
2015-05-28 12:39:40 +0000

    add filterView

--- Ayamy App/scripts/app.js
+++ Ayamy App/scripts/app.js
@@ -1,9 +1,8 @@
 var app = (function () {
 
-    var mobileApp = {},
-        dataSource, price, total = 0,
-        count = 0,
-        countEl;
+    var dataSource,
+        mobileApp = {},
+        purchase = [];
 
     var groupedData = [
         {
@@ -73,72 +72,120 @@ var app = (function () {
                 data: groupedData,
                 group: "letter"
             }),
+            filterable: {
+                field: "name",
+                operator: "startswith",
+                placeholder: "Поиск..."
+            },
             template: $("#main-template").html(),
             headerTemplate: "<h4 style='color:gray'>${value}</h4>"
         });
     }
 
+    var Item = function (id, price) {
+        this.id = id;
+        this.price = price;
+        this.count = 1;
+        this.total = price;
+    }
+
+    function agregate() {
+        //Подводим итог
+        //Суммируем все елементы массива и заносим в корзину
+        var sum = 0;
+        for (var j = 0; j < purchase.length; j++) {
+            sum += purchase[j].total;
+        }
+        $("#basket").text(sum);
+    }
+
     function plus(e) {
-        //Получаем текущий элемент с кол-вом
-        countEl = $(e.target).prev();
-        //Получаем текущее кол-во
-        count = +$(e.target).prev().text();
-        price = +$(e.target).prev().data("price");
-        //Прибавляем 1
-        count++;
-        //Заносим в текущий элемент
-        $(e.target).prev().text(count);
-        //Вычитаем из общей стоимости текущий товар
-        total += price;
-        // //Заносим с текущим кол-вом
-        //total += price * count;
-        $("#basket").text(total);
+        //Получаем текущий объект по id
+        var id = $(e.target).prev().data("id");
+        for (var i = 0; i < purchase.length; i++) {
+            if (purchase[i].id === id) {
+                //Увеличиваем на кол-во на 1
+                purchase[i].count++;
+                //Заносим в текущий элемент
+                $(e.target).prev().text(purchase[i].count);
+                //Устанавливаем новую стоимость
+                purchase[i].total = purchase[i].count * purchase[i].price;
+                break;
+            }
+        }
+        agregate();
     }
 
     function minus(e) {
-        countEl = $(e.target).next();
-        count = +$(e.target).next().text();
-        price = +$(e.target).next().data("price");
-        if (count > 1) {
-            count--;
-            $(e.target).next().text(count);
-            //Вычитаем из общей стоимости текущий товар
-            total -= price;
-            $("#basket").text(total);
+        //Получаем текущий объект по id
+        var id = $(e.target).next().data("id");
+        for (var i = 0; i < purchase.length; i++) {
+            if (purchase[i].id === id) {
+                if (purchase[i].count > 1) {
+                    //Уменьшаем кол-во на 1
+                    purchase[i].count--;
+                    //Заносим в текущий элемент
+                    $(e.target).next().text(purchase[i].count);
+                    //Устанавливаем новую стоимость
+                    purchase[i].total = purchase[i].count * purchase[i].price;
+                    break;
+                }
+            }
         }
+        agregate();
     }
 
-    var purchase = [];
-    var item = {};
-
     function doPrice(e) {
-
-        //Текущая цена
-        price = $(e.target).data("price");
+        
+        var curId = $(e.target).data("id");
+        
         //Если цена выделена
         if (!$(e.target).data("on")) {
-            //Здесь заносить в масив покупок id и цену
-            //Общая стоимость
-            total += price;
+			var item = new Item(curId, $(e.target).data("price"))
+            purchase.push(item);
+            //price += item.price;
+            //Добавляем сразу в корзину
+            agregate();
             $(e.target).data("on", true);
         } else {
-            //Здесь находить по id кол-во покупок по текущему итему
-            if (total - price * count > 0) {
-                total -= price * count;
-                $(countEl).text(1);
-            } else {
-                total = 0;
+            //Ищем полное кол-во по текущему итему
+            for (var i = 0; i < purchase.length; i++) {
+                if (curId === purchase[i].id) {
+                    //Удаляем итем из общего массива
+                    purchase.splice(i, 1);
+                    $("span[data-id='" + curId + "']").text(1);
+                }
             }
+            agregate();
             $(e.target).data("on", false);
         }
 
-        $("#basket").text(total);
+        //Применяем эффекты
         $(e.target).toggleClass("prices-clicked");
-        $("#" + $(e.target).data("id")).slideToggle("normal");
+        $("#" + curId).slideToggle("normal");
+        $("#img" + curId).fadeToggle("normal");
+        $("p.description").toggleClass("description-left");
+    }
+
+    function toBasket () {
+        mobileApp.navigate("#basket", "slide");
     }
     
-    function toBasket() {
-        mobileApp.navigate("#basket", "slide");
+    function toFilter () {
+        mobileApp.navigate("#filterView", "slide");
+    }
+    
+    function filterViewInit () {
+        var listviews = this.element.find("ul.km-listview");
+
+        $("#select-filter").kendoMobileButtonGroup({
+            select: function(e) {
+                listviews.hide()
+                         .eq(e.index)
+                         .show();
+            },
+            index: 0
+        });
     }
 
     document.addEventListener("deviceready", initialize);
@@ -148,7 +195,9 @@ var app = (function () {
         doPrice: doPrice,
         p: plus,
         m: minus,
-        goBasket: toBasket
+        goBasket: toBasket,
+        goFilter: toFilter,
+        filterViewInit: filterViewInit
     }
 
 }());
