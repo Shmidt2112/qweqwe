f521e76868ec7e74cf54c02c28c48e1643753176
Damir Shmidt <damirkin11@mail.ru>
2015-05-31 13:25:16 +0000

    good version, allwork

--- Ayamy App/scripts/app.js
+++ Ayamy App/scripts/app.js
@@ -95,7 +95,7 @@ var app = (function () {
                 placeholder: "поиск..."
             },
             template: $("#main-template").html(),
-            headerTemplate: "<h4 style='color:gray'>${value}</h4>"
+            headerTemplate: "<h4 style='color:gray; margin: 5px;'>${value}</h4>"
         });
     }
 
@@ -110,9 +110,10 @@ var app = (function () {
     }
 
     function plus(e) {
-        //Получаем текущий объект по id
+        //Получаем текущий объект по id    
         for (var i = 0; i < purchase.length; i++) {
             if (purchase[i].id === $(e.target).prev().data("id")) {
+                var a = $(e.target).prev();
                 //Увеличиваем на кол-во на 1
                 purchase[i].count++;
                 //Заносим в текущий элемент
@@ -136,67 +137,111 @@ var app = (function () {
                     //Устанавливаем новую стоимость
                     purchase[i].total = purchase[i].count * purchase[i].price;
                     break;
+                } else {
+                    var button = $(e.target).closest('li').find('a.prices');
+                    //$("#" + $(e.target).next().data("id")).slideToggle("normal");
+                    //var a = $(e.target).data("on");
+                    //Ищем полное кол-во по текущему итему
+                    for (var i = 0; i < purchase.length; i++) {
+                        //if (curId === purchase[i].id) {
+                        if ($(e.target).next().data("id") === purchase[i].id) {
+                            //Удаляем итем из общего массива
+                            purchase.splice(i, 1);
+                            $("span[data-id='" + $(e.target).next().data("id") + "']").text(1);
+                        }
+                    }
+                    //agregate();
+                    $(button).data("on", false);
+                    var directionAnimate = parseInt($("#" + $(e.target).next().data("direct")));
+                    if (directionAnimate == 0) {
+                        $("#" + $(e.target).next().data("id")).fadeToggle("fast");
+                        //setTimeout(function(){ $("#" + $(e.target).next().data("id")).fadeToggle("fast");}, 300);
+                        //$(button).css("display", "block");
+                        //setTimeout(function(){ $(button).fadeToggle("fast");}, 300);
+                        setTimeout(function () {
+                            $(button).fadeToggle("fast");
+                        }, 400);
+                    } else {
+
+                        $("#" + $(e.target).next().data("id")).fadeToggle("fast");
+                        setTimeout(function () {
+                            $(button).fadeToggle("fast");
+                        }, 400);
+                        //$(button).fadeToggle("fast");
+                        //$(button).css("display", "block");
+                    }
+                    timer = setInterval(animationImg, 20, $(e.target).next().data("id"));
                 }
             }
         }
         agregate();
     }
-	
-	var directionAnimate=0;
+
     function doPrice(e) {
         curId = $(e.target).data("id");
         //Если цена выделена
         if (!$(e.target).data("on")) {
+            var a = $(e.target).data("on");
             var item = new Item(curId, $(e.target).data("price"))
             purchase.push(item);
             //price += item.price;
             //Добавляем сразу в корзину
             agregate();
             $(e.target).data("on", true);
-        } else {
-            //Ищем полное кол-во по текущему итему
-            for (var i = 0; i < purchase.length; i++) {
-                if (curId === purchase[i].id) {
-                    //Удаляем итем из общего массива
-                    purchase.splice(i, 1);
-                    $("span[data-id='" + curId + "']").text(1);
-                }
-            }
-            agregate();
-            $(e.target).data("on", false);
         }
+        //else 
+        //{
+        //    var a = $(e.target).data("on");
+        //Ищем полное кол-во по текущему итему
+        //    for (var i = 0; i < purchase.length; i++) {
+        //        if (curId === purchase[i].id) {
+        //            //Удаляем итем из общего массива
+        //            purchase.splice(i, 1);
+        //            $("span[data-id='" + curId + "']").text(1);
+        //        }
+        //    }
+        //    agregate();
+        //   $(e.target).data("on", false);
+        //}
 
         //Применяем эффекты
-        $(e.target).toggleClass("prices-clicked");
+        //$(e.target).toggleClass("prices-clicked");
+        $(e.target).css("display", "none");
         //$(e.target).slideToggle("normal"); 
-        $("#" + curId).slideToggle("normal");
-        //$("#img" + curId).fadeToggle("normal");  
-        timer = setInterval( animation, 10, curId);
+        //$("#" + curId).slideToggle("normal");
+        $("#" + curId).css("display", "block");
+        //$("#img" + curId).fadeToggle("normal");   
+        timer = setInterval(animationImg, 20, curId);
     }
-    function animation(id) {        
-        var obj = $("#img" + curId);
-        var w = parseInt($(obj).css('width'));  ///определим текущую ширину блока 
-        
-        if(w == 75 && directionAnimate==1){	///если нет, разрушим интервал (перестанем вызывать функцию animation()) 
-            directionAnimate = 0;
-            clearInterval(timer);
+
+    function animationImg(id) {
+        var obj = $("#img" + id);
+        var w = $(obj).width();
+        var directionAnimate = parseInt($("#img" + id).data("direct"));
+
+        if (w > 0 && directionAnimate == 0) { ///если ширина >  0, уменьшаем ширину блока на 5px 
+            $(obj).css('width', w - 15 + "px");
+            $(obj).css('height', 75 + "px");
         }
-        
-        if (w >0 && directionAnimate==0) { 	///если ширина >  0, уменьшаем ширину блока на 5px
-            $(obj).css('width', w - 5 + "px");   
-            $(obj).css('height', 75 +"px");
-        }  
-        
-        if (w<75 && directionAnimate==1 ) ///если ширина < 75, уменьшаем ширину блока на 5px
-        { 
-            $(obj).css('width', w + 5 + "px");   
-            $(obj).css('height', 75 +"px"); 
+
+        if (w < 75 && directionAnimate == 1) ///если ширина < 75, уменьшаем ширину блока на 5px
+        {
+            $(obj).css('width', w + 15 + "px");
+            $(obj).css('height', 75 + "px");
         }
-        
-        if(w == 0 && directionAnimate==0){	///если нет, разрушим интервал (перестанем вызывать функцию animation()) 
+
+        if (w == 0 && directionAnimate == 0) { ///если нет, разрушим интервал (перестанем вызывать функцию animation()) 
             directionAnimate = 1;
+            $("#img" + id).data("direct", directionAnimate);
             clearInterval(timer);
-        }     
+        }
+
+        if (w == 75 && directionAnimate == 1) { ///если нет, разрушим интервал (перестанем вызывать функцию animation())  
+            directionAnimate = 0;
+            $("#img" + id).data("direct", directionAnimate);
+            clearInterval(timer);
+        }
+
     }
 
     function toMain() {
